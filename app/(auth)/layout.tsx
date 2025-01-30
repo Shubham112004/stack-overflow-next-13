4564a4fc4503564ec410949f7f69e8fe8546fdb0
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2025-01-30 12:03:19 +0530

    add auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+
+export default function Layout({ children, }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <main className="flex min-h-screen w-full items-center justify-center">
+            {children}
+        </main>
+    );
+}
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+export default function AskQuestion() {
+    return (
+        <main>
+            ask question
+        </main>
+    )
+}
